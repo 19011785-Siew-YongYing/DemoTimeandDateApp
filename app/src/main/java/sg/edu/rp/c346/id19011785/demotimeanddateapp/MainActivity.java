fbc88fb2a4968dab0dd5e8f66074f04893ca304b
19011785-SiewYongYing <19011785@myrp.edu.sg>
2021-05-10 10:02:35 +0800

    Made changes

--- app/src/main/java/sg/edu/rp/c346/id19011785/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19011785/demotimeanddateapp/MainActivity.java
@@ -30,7 +30,21 @@ public class MainActivity extends AppCompatActivity {
         btnDisplayTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                int hr = tp.getCurrentHour();
+                int min = tp.getCurrentMinute();
+                //tvDisplay.setText("Time is " + timeHr + ":" + min);
+                tvDisplay.setText(String.format("Time is %d:%d", hr, min));
+            }
+        });
+
+        btnDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int mth = dp.getMonth();
+                int yr = dp.getYear();
+                int day = dp.getDayOfMonth();
+                //tvDisplay.setText("Date is " + day + "/" + mth + "/" + yr); // Display format D/M/Y
+                tvDisplay.setText(String.format("Date is %d/%d/%d", day, mth, yr));
 
             }
         });
