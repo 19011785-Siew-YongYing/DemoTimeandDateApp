bcd9604456474572dde9335a6c27e0fd47c1c1da
19011785-SiewYongYing <19011785@myrp.edu.sg>
2021-05-10 09:34:33 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19011785/demotimeanddateapp/MainActivity.java
@@ -0,0 +1,39 @@
+package sg.edu.rp.c346.id19011785.demotimeanddateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
+    TextView tvDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textView);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+
+            }
+        });
+
+    }
+}
